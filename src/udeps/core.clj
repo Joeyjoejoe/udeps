946020a52c7e51420d1ee1487e09d2f18d1f2d4d
victor Defontaine <vikthor.d@gmail.com>
2023-02-02 16:25:14 +0100

    Simplify code and beautify logs

--- src/udeps/core.clj
+++ src/udeps/core.clj
@@ -10,30 +10,35 @@
   (ns my.ns
   (:require [udeps.core :as udeps]))
 
-  ;; Inject `foo` and `bar` functions in `my.ns` namespace
-  (udeps/inject! :udeps/foo :udeps/bar)
+  (udeps/inject!
+    ;; Inject `foo` and `bar` functions in `my.ns` namespace
+    :udeps/foo :udeps/bar
+
+    ;; Inject `doo` aliased as `bop` in `my.ns` namespace
+    [:udesp/doo :as :bop]
+  )
 
   ;; Which you can use as building blocks for your functions
-  (defn my-fn []
-    (let [x (foo)
-          y (bar :whatever)]
+  (defn my-fn [n]
+    (let [x (-> n foo bop)
+          y (bar :y)]
       (+ x y)))
   ```
   "
   [& deps]
-  (let [sources (config/init)]
-    (log/inject-header (str *ns*))
+  (let [conf (config/init)]
     `(do
-       ~@(map (fn [dep]
-                (if-let [data (parser/read-dep! dep sources)]
-                  (let [fn-name     (:name data)
-                        body      (:body data)
-                        fn-fullname  (str *ns* "/" (name fn-name))]
-                    (if (-> fn-fullname symbol resolve)
-                      (log/error dep (str "#'" fn-fullname " already defined"))
-                      (do
-                        (if (:cache data)
-                          (log/cache fn-fullname)
-                          (log/success fn-fullname))
-                        `(def ~(symbol fn-name) ~(read-string body)))))))
-              deps))))
+       ~@(map
+           (fn [dep]
+             (binding [udeps.logs/verbose? (:cfg/verbose conf)]
+               (if-let [data (parser/read-dep! dep conf)]
+                 (let [fn-name     (:name data)
+                       body      (:body data)
+                       fn-fullname  (str *ns* "/" (name fn-name))]
+
+                   (if (-> fn-fullname symbol resolve)
+                     (log/error dep (str "#'" fn-fullname " already defined"))
+                     (do
+                       (log/success fn-fullname (:source data))
+                       `(def ~(symbol fn-name) ~(read-string body))))))))
+           deps))))
--- src/udeps/logs.clj
+++ src/udeps/logs.clj
@@ -1,42 +1,33 @@
 (ns udeps.logs)
 
+;; See color ant text formating ref:
+;;https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
+
+(def ^:dynamic verbose? true)
+(def head "\u001B[38;5;238m(µdeps)")
+
 (defn error
   [title msg]
-  (let [icon     "\u2718"
-        red      "\u001B[31m"
-        dark-red "\u001B[38;5;124m"
-        grey     "\u001B[38;5;243m"
-        tab   " "
-        reset    "\u001B[0m"]
-    (println tab red icon grey title dark-red msg reset)))
+  (let [icon      "\u2718"
+        red       "\u001B[38;5;9m"
+        red-bold  "\u001B[38;5;9;1m"
+        dark-red  "\u001B[38;5;214m"
+        grey      "\u001B[38;5;243m"
+        dark-grey "\u001B[38;5;238m"
+        reset     "\u001B[0m"]
+
+    (println head (str red icon) (str grey *ns*) (str red-bold title reset) (str dark-red msg reset))))
+
 
 (defn success
-  [fname]
-  (let [icon  "\u2714"
-        green "\u001B[32m"
-        grey  "\u001B[38;5;243m"
-        tab   " "
-        reset "\u001B[0m"]
-   (println tab green icon grey (str "#'" fname) reset)))
+  [fname source]
+  (if verbose?
+    (let [icon       "\u2714"
+          green      "\u001B[32m"
+          green-bold "\u001B[32;1m"
+          grey       "\u001B[38;5;243m"
+          dark-grey  "\u001B[38;5;238m"
+          blue-bold  "\u001B[38;5;34;1m"
+          reset      "\u001B[0m"]
 
-
-(defn inject-header
-  [nsname]
-  (let [icon   "\u2605"
-        gold   "\u001B[38;5;229m"
-        orange "\u001B[38;5;214;1m"
-        grey  "\u001B[38;5;243m"
-        reset "\u001B[0m"
-        nsname (str "(ns " nsname ")")]
-  (println)
-  (println gold icon (str orange " µdeps") "\u27A5" (str grey nsname reset))
-  (println)))
-
-(defn cache
-  [fname]
-  (let [icon  "\u2714"
-        blue  "\u001B[38;5;27m"
-        grey  "\u001B[38;5;243m"
-        tab   " "
-        reset "\u001B[0m"]
-   (println tab blue icon grey (str "#'" fname) reset)))
+        (println head (str green icon) (str grey *ns*) (str green-bold "#'" fname reset) (str dark-grey source reset)))))
